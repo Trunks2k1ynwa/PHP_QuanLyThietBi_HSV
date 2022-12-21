ce8a79a58b7f94fa244e812a471e727bd3bf597b
Trunks <Trungthekop123@gmail.com>
2022-12-21 20:10:25 +0700

    commit change

---
+++ .history/Layout/Script/ListUser_20221221201008.js
@@ -0,0 +1,12 @@
+const btnAdd = document.querySelector('.addnew');
+const addview = document.querySelector('.add-account');
+const blur = document.querySelector('.blur');
+const btnClose = document.querySelector('.add-account .btn_close');
+btnAdd.addEventListener('click', function(){
+    addview.classList.add('active');
+    blur.classList.add('active');
+})
+btnClose.addEventListener('click',function () {
+    addview.classList.remove('active');
+    blur.classList.remove('active');
+})
